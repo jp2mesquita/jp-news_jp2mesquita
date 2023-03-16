88b25247f8b2f467e39274e72aa264ccaec77451
João Paulo Mesquita <jp-mesquita@live.com>
2023-03-16 12:13:35 -0300

    feat: setup provider to use darkmode function

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import Header from './Header'
+import Providers from './Providers'
 
 export const metadata = {
   title: 'Next.js',
@@ -15,10 +16,12 @@ export default function RootLayout({
     <html lang="en">
 
       <body className='bg-gray-100 dark:bg-zinc-900 transition-all duration-700'>
-        <Header />
-        <div className='max-w-6xl mx-auto'>
-          {children}
-        </div>
+        <Providers>
+                <Header />
+                <div className='max-w-6xl mx-auto'>
+                  {children}
+                </div>
+        </Providers>
       </body> 
     </html>
   )
