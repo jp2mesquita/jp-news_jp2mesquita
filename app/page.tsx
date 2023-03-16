ed9e60635ebcf78b42aca4d288d7fd0a4b7360b2
João Paulo Mesquita <jp-mesquita@live.com>
2023-03-16 10:42:42 -0300

    feat: calling the fetchNews passing all the categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+import { categories } from '../constants'
+import { fecthNews } from '../utils/fetchNews'
+
 export const metadata = {
   title: 'TheBrabus News'
 }
 
-export default function Home (){
+export default async function Home (){
+
+  // const category = categories.join(',')
+
+  const news: NewsResponse = await fecthNews(categories.join(','))
+
+  console.log(news)
   return(
-    <div>Homepage</div>
+    <div>
+    {/* NewsList news */}
+
+    </div>
   )
 }
